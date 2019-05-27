727b659b37d1686e8ce8733cbdb2801cb8bb59fc
David Fan <davidfalex@gmail.com>
2019-05-27 15:02:07 +0100

    moved some stuff around

--- bin/setup_jenkins.sh
+++ bin/setup_jenkins.sh
@@ -13,7 +13,7 @@ CLUSTER=$3
 echo "Setting up Jenkins in project ${GUID}-jenkins from Git Repo ${REPO} for Cluster ${CLUSTER}"
 
 # Set up Jenkins with sufficient resources
-oc new-app jenkins-persistent --param ENABLE_OAUTH=true --param MEMORY_LIMIT=2Gi --param VOLUME_CAPACITY=6Gi --param DISABLE_ADMINISTRATIVE_MONITORS=true -n ${GUID}-jenkins
+oc new-app jenkins-persistent --param ENABLE_OAUTH=true --param MEMORY_LIMIT=2Gi --param VOLUME_CAPACITY=4Gi --param DISABLE_ADMINISTRATIVE_MONITORS=true -n ${GUID}-jenkins
 
 oc set resources dc/jenkins --limits=memory=2Gi,cpu=2 --requests=memory=2Gi,cpu=1 -n ${GUID}-jenkins
 
